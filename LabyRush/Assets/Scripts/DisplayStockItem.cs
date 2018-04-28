5e01a0111327e00273b45218b1e42785cc85412f
MargotW <m.puyjalon@gmail.com>
2018-04-28 18:09:24 +0200

    Add display stock item

---
+++ LabyRush/Assets/Scripts/DisplayStockItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayStockItem : MonoBehaviour
+{
+    public Texture bomb;
+    public Texture wall;
+    public Texture jump;
+    public Texture empty;
+    public Texture item;
+
+    void Start () {
+        item = empty;
+	}
+	
+	void Update () {
+        if (gameObject.transform.GetChild(0).tag == "bomb") item = bomb;
+        else if (gameObject.transform.GetChild(0).tag == "wall") item = wall;
+        else if (gameObject.transform.GetChild(0).tag == "jump") item = jump;
+        else item = empty;
+    }
+
+    void OnGUI() {
+        GUILayout.BeginArea(new Rect((Screen.width/2), (Screen.height-50), 50, 50));
+        GUILayout.Box(item);
+        GUILayout.EndArea();
+    }
+}
