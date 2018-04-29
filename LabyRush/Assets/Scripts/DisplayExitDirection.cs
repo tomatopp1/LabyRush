a8fc3a19d0e824776d262df000cc80205ac67142
MargotW <m.puyjalon@gmail.com>
2018-04-29 16:57:32 +0200

    Add a directional arrow

---
+++ LabyRush/Assets/Scripts/DisplayExitDirection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayExitDirection : MonoBehaviour {
+
+    public GUIStyle customStyle;
+    public Transform exit;
+    public GameObject arrow;
+	
+	// display the arrow toward the exit
+	void Update () {
+        arrow.transform.LookAt(exit);
+    }
+}
