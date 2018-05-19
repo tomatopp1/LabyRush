7c9305746cc57834241d1f940688c5c3218122c1
Unknown <finouxnicolas@gmail.com>
2018-05-19 17:03:25 +0200

    Grosse MAJ: Depart + option lobby(voir description)
    Options au lobby pour sélectionner le nombre max de joueur pour la partie. ensuite une fois que dans le laby chargé et que le nombre max de joueur est arrivé, au bout de 3 seconde, le compte a rebours démarre, une fois celui ci fini, le mur est détruit et la partie commence

---
+++ LabyRush/Assets/Scripts/synchro_start.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class synchro_start : MonoBehaviour
+{
+    public int timeToStart;
+    public int ReadyTime;
+    private bool ready;
+
+    private float realTime;
+    private int nb_player_max;
+    private TextMesh txt;
+    private DisplayTimer dt;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ready = false;
+        realTime = ReadyTime;
+        nb_player_max = PhotonNetwork.room.MaxPlayers;
+        txt = GameObject.Find("Text_countdown").GetComponent<TextMesh>();
+        txt.text = "";
+        dt = GameObject.Find("Scripts").GetComponent<DisplayTimer>();
+        dt.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (PhotonNetwork.room.playerCount == nb_player_max)
+        {
+            realTime -= Time.deltaTime;
+            if(ready != true)
+            {
+                if (realTime <= 0)
+                {
+                    ready = true;
+                    realTime = timeToStart;
+                }
+            }
+            else
+            {
+                txt.text = (realTime % 60).ToString("00");
+
+                if ((realTime % 60) <= 0)
+                {
+                    Destroy(gameObject);
+                    dt.enabled = true;
+                }
+            }
+        }      
+	}
+}
